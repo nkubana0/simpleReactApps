ba495e4f4e2360047000bb2e6e62824b6c1dfc1b
nkubana0 <i.shema@alustudent.com>
2024-04-23 19:50:57 +0200

    Added gemini API

---
+++ Gemini-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import MainBody from "./Components/Main-body/MainBody";
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <MainBody />
+    </>
+  );
+};
+
+export default App;
